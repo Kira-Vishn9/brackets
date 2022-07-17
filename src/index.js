efcad80633997c8448ff8ed9a927f37f61f54442
222 <a222@MacBook-Air.local>
2022-07-17 08:39:33 -0400

    feat: done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  arr = str.split('');
+
+  function checkBrackets(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      for (let k = 0; k < bracketsConfig.length; k++) {
+        if (arr[i] === bracketsConfig[k][0] && arr[i + 1] === bracketsConfig[k][1]) {
+          arr.splice(i, 2);
+          checkBrackets(arr);
+        }
+      }
+    }
+  }
+
+  checkBrackets(arr);
+
+  if (arr.length === 0) {
+    return true;
+  }
+
+  return false;
 }
